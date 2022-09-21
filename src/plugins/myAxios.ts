539720da194de5e2643ee973a1d37bb48b42222e
陈健 <1216912253@qq.com>
2022-09-21 23:34:46 +0800

    https

--- src/plugins/myAxios.ts
+++ src/plugins/myAxios.ts
@@ -5,7 +5,7 @@ const myAxios = axios.create({
   baseURL:
     // @ts-ignore
     process.env.NODE_ENV === "production"
-      ? "http://124.222.202.111/api"
+      ? "http://127.0.0.1/api"
       : "http://192.168.1.107:8089",
 });
 
