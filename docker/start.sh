bbdf8dd0294ad7f20f767b85308e87d178924efd
zima <1216912253@qq.com>
2023-12-11 23:14:25 +0800

    优化docker部署

--- docker/start.sh
+++ docker/start.sh
@@ -7,20 +7,20 @@ PROJECT_ROOT="/opt/project/chentool-app"
 cd $PROJECT_ROOT || exit 1
 
 # 从 Git 拉取最新代码
-echo "update code..."
+echo "更新git代码===>"
 git pull --rebase || exit 1
 
 # 设置 Docker Compose 文件的路径
 COMPOSE_FILE=./docker/docker-compose.yml
 
 # 构建 Docker 镜像
-echo "Building Docker image..."
+echo "制作docker镜像===>"
 docker compose -f $COMPOSE_FILE build || exit 1
 
 # 检查并停止已运行的容器
-echo "Stopping existing containers..."
+echo "停止已运行的容器===>"
 docker compose -f $COMPOSE_FILE down || exit 1
 
 # 启动新的容器
-echo "Starting new containers..."
+echo "开始部署容器===>"
 docker compose -f $COMPOSE_FILE up -d || exit 1
